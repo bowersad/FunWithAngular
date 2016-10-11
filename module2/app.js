c14187d5a00808c708e50c12a49a4460a7191e1d
bowersad <bowersad@gmail.com>
2016-10-11 08:45:07 -0400

    Going back to clean up module 2

--- module2/app.js
+++ module2/app.js
@@ -1,11 +1,29 @@
 (function () {
-	'use strict';
+'use strict';
 
-	angular.module('module2',[])
-	.controller('module2controller',module2controller);
+angular.module('module2',[])
+.controller('module2controller',module2controller)
+.service('MenuItemService',MenuItemService)
+.constant('MenuItemApiURL','https://davids-restaurant.herokuapp.com')
 
+module2controller.$inject = ['MenuItemService']
+function module2controller(MenuItemService) {
+	
+};
 
-	function module2controller() {
-		
+MenuItemService.$inject = ['$http','MenuItemApiURL'];
+function MenuItemService ($http, MenuItemApiURL) {
+	var service = this;
+
+	service.getMenuItems = function (categoryCode) {
+		var response = $http({
+			method : "GET",
+			URL : MenuItemApiURL + "/menu_items.json",
+			params : {
+				category : categoryCode
+			}
+		});
+		return response;
 	};
-});
+};
+})();
