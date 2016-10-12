c0b29d817083112f4c4efe731600491f0c74bd24
bowersad <bowersad@gmail.com>
2016-10-12 09:03:40 -0400

    Showing results of category list

--- module2/app.js
+++ module2/app.js
@@ -8,17 +8,45 @@ angular.module('module2',[])
 
 module2controller.$inject = ['MenuItemService']
 function module2controller(MenuItemService) {
-	
+	var menu = this;
+
+	var promise = MenuItemService.getCategories();
+
+	promise.then(function (response) {
+		menu.categories = response.data;
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+
+	menu.getItemsByCategory = function(categoryCode) {
+		var promise = MenuItemService.getMenuItems(categoryCode);
+		console.log('Yup');
+
+		promise.then(function (response) {
+			console.log(response.data);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+	};
 };
 
 MenuItemService.$inject = ['$http','MenuItemApiURL'];
 function MenuItemService ($http, MenuItemApiURL) {
 	var service = this;
 
+	service.getCategories = function () {
+		var response = $http({
+			method : "GET",
+			url : MenuItemApiURL + "/categories.json"
+		});
+		return response;
+	};
 	service.getMenuItems = function (categoryCode) {
 		var response = $http({
 			method : "GET",
-			URL : MenuItemApiURL + "/menu_items.json",
+			url : MenuItemApiURL + "/menu_items.json",
 			params : {
 				category : categoryCode
 			}
