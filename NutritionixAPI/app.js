0b80c51383c35b77dc3e85ca19de8a9ad4b7aec1
bowersad <bowersad@gmail.com>
2016-10-14 10:30:59 -0400

    Adding API Search

--- NutritionixAPI/app.js
+++ NutritionixAPI/app.js
@@ -10,6 +10,7 @@ NutritionixController.$inject = ['NutritionixService'];
 function NutritionixController(NutritionixService) {
 	var app = this;
 
+
 	var promise = NutritionixService.GetItembyID("513fc9cb673c4fbc2600536a");
 
 	promise.then(function (response) {
@@ -20,11 +21,32 @@ function NutritionixController(NutritionixService) {
 	.catch(function (error) {
 		console.log(error)
 	});
+
+	app.ViewFoodItem = function() {
+
+	}
+
+	app.SearchTerm = "";
+
+	app.SearchAPI = function() {
+		console.log(app.SearchTerm);
+		var promise = NutritionixService.SearchItems(app.SearchTerm);
+
+	    promise.then(function (response) {
+	      console.log(response.data);
+	      app.ReturnItems = response.data;
+	    })
+	    .catch(function (error) {
+	      console.log(error);
+	    })
+	};
 };
 
 NutritionixService.$inject = ['$http','NutritionixRootPath']
 function NutritionixService($http,NutritionixRootPath) {
 	var service = this;
+
+
 
 	service.GetItembyID = function(itemId) {
 		var response = $http({
@@ -39,6 +61,20 @@ function NutritionixService($http,NutritionixRootPath) {
 
 		return response;
 	};
+
+	service.SearchItems = function(searchTerm) {
+		var response = $http({
+			method : "GET",
+			url : NutritionixRootPath + "/search",
+			params : {
+				id : searchTerm,
+				appId : "a7342583",
+				appKey : "30aa58fbc7dbc964d1384f9a98bc4b2d"
+			}
+		});
+
+		return response;
+	}
 };
 
 })();
