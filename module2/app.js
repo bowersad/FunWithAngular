3cb5a51b846449d47bd561fcf3764bded7c3cf42
bowersad <bowersad@gmail.com>
2016-10-17 09:07:32 -0400

    Returning to Module 2

--- module2/app.js
+++ module2/app.js
@@ -10,26 +10,9 @@ module2controller.$inject = ['MenuItemService']
 function module2controller(MenuItemService) {
 	var menu = this;
 
-	var promise = MenuItemService.getCategories();
+	menu.shoppingList = MenuItemService.getShoppingList();
+	console.log(menu.shoppingList.shopping_items);
 
-	promise.then(function (response) {
-		menu.categories = response.data;
-	})
-	.catch(function (error) {
-		console.log(error);
-	});
-
-	menu.getItemsByCategory = function(categoryCode) {
-		var promise = MenuItemService.getMenuItems(categoryCode);
-		console.log('Yup');
-
-		promise.then(function (response) {
-			console.log(response.data);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
-	};
 };
 
 MenuItemService.$inject = ['$http','MenuItemApiURL'];
@@ -52,6 +35,21 @@ function MenuItemService ($http, MenuItemApiURL) {
 			}
 		});
 		return response;
+	};
+
+	service.getShoppingList = function () {
+		return {"shopping_items":[{"id": 1, "name" : "eggs", "quantity" : 12},
+								{"id": 2, "name" : "cheese", "quantity" : 3},
+								{"id": 3, "name" : "black beans", "quantity" : 1},
+								{"id": 4, "name" : "pinto bean", "quantity" : 2},
+								{"id": 5, "name" : "onions", "quantity" : 2},
+								{"id": 6, "name" : "peppers", "quantity" : 1},
+								{"id": 7, "name" : "avocado", "quantity" : 3},
+								{"id": 8, "name" : "tomatoes", "quantity" : 6},
+								{"id": 9, "name" : "tortillas", "quantity" : 6},
+								{"id": 10, "name" : "spice", "quantity" : 10},
+								{"id": 11, "name" : "oil", "quantity" : 1},
+								{"id": 12, "name" : "lime juice", "quantity" : 2}]}
 	};
 };
 })();
